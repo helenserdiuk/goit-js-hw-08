a06895bcd06fb9c933efd73ba88118fb3aa8049e
Elena S <helenserdiuk@gmail.com>
2022-05-27 20:11:42 +0300

    editing

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,7 @@
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-
-import throttle from 'lodash.throttle';
 
 const onPlay = function ({ seconds }) {
   localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
