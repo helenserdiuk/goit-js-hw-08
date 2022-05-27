8ca83887cf8db9d991b73dd45defa08af78e2337
Elena S <helenserdiuk@gmail.com>
2022-05-27 22:05:28 +0300

    new puck

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,6 +4,8 @@ import { galleryItems } from './gallery-items';
 import simpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+// console.log(galleryItems);
+
 const gallery = document.querySelector('.gallery');
 
 function createGalleryCard(array) {
