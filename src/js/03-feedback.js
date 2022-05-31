44120dc0fe3c8176cb78cf081ff38113947c645c
Elena S <helenserdiuk@gmail.com>
2022-05-31 04:19:56 +0300

    Add object in console

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -31,7 +31,7 @@ const onCotactFormElChange = event => {
 
 const onContactFormSubmit = event => {
   event.preventDefault();
-
+  console.log(localStorageApi.load(CONTACT_FORM_LOCAL_STORAGE_KEY));
   localStorageApi.remove(CONTACT_FORM_LOCAL_STORAGE_KEY);
   event.currentTarget.reset();
 };
